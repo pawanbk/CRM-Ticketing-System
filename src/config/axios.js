6fb314f305cd86fd852b6cebe887af7fdabd1cb4
PAWAN BK <50671233+pawanbk@users.noreply.github.com>
2024-03-21 22:37:11 +1100

    Revert "merging to develop"

--- src/config/axios.js
+++ src/config/axios.js
@@ -7,9 +7,6 @@ const refreshToken = AuthService.getRefreshToken();
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3001/v1/",
-  headers: {
-    Authorization: accessToken,
-  },
 });
 
 axiosInstance.interceptors.request.use(
@@ -54,7 +51,7 @@ axiosInstance.interceptors.response.use(
           // return Navigate({ to: "/login" });
         }
       } else {
-        console.error("refere token expired");
+        console.error("refere=sh token expired");
         // return Navigate({ to: "/login" });
       }
     }
