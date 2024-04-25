359aa751a54d15d2cd0aff434c2b3e91c7e1142c
Pawan B K <shiwanbk@gmail.com>
2024-04-25 19:54:58 +1000

    ui updated

--- src/component/comment/CommentInput.jsx
+++ src/component/comment/CommentInput.jsx
@@ -1,28 +1,27 @@
-import { Form } from "react-bootstrap"
 import './CommentInput.css'
 import TextareaAutosize from 'react-textarea-autosize';
 
 const CommentInput = (props) => {
-    return(
-    <div className="comment-input">
-       <TextareaAutosize 
-        className="w-100 p-2 border rounded mt-1"
-        as="textarea" 
-        placeholder="write a comment here..." 
-        name="commentInput" 
-        value={props.commentInput} 
-        onChange={props.change}
-        onKeyPress={(e) => e.key === 'Enter' && props.addComment()}
-        />
-        <div className="float-right  my-1">
-            {props.cancelBtn && 
-            <button className="btn btn-sm btn-secondary " onClick={props.hideCommentInput}>Cancel</button>}
-        
-            {/* <button className="btn btn-sm btn-primary mx-1" onClick={props.addComment}>Comment</button> */}
+    return (
+        <div className="comment-input">
+            <TextareaAutosize
+                className="w-100 p-2 border rounded mt-1"
+                as="textarea"
+                placeholder="write a comment here..."
+                name="commentInput"
+                value={props.commentInput}
+                onChange={props.change}
+                onKeyPress={(e) => e.key === 'Enter' && props.addComment()}
+            />
+            <div className="float-right  my-1">
+                {props.cancelBtn &&
+                    <button className="btn btn-sm btn-secondary " onClick={props.hideCommentInput}>Cancel</button>}
+
+                {/* <button className="btn btn-sm btn-primary mx-1" onClick={props.addComment}>Comment</button> */}
+            </div>
+
+
         </div>
-           
-            
-    </div>
     )
 }
 
