1fbee843ce7825244ed7c4daba3469f53c80cc69
Pawan B K <shiwanbk@gmail.com>
2024-06-08 21:26:45 +1000

    edit modal

--- src/component/comment/edit-modal/EditComment.tsx
+++ src/component/comment/edit-modal/EditComment.tsx
@@ -5,11 +5,15 @@ import TicketService from "../../../api/TicketServices";
 import { CheckCircle } from "react-bootstrap-icons";
 import './style.css'
 import { useState } from "react";
+import { capitalize } from "lodash";
+import moment from "moment";
 
-const EditComment = ({show, setShow, comment, fetchTicket}) => {
+const EditComment = ({ show, setShow, comment, fetchTicket }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [commentInput, setCommentInput] = useState("");
     const [success, setSuccess] = useState(false);
+    const secondsInDay = 24 * 60 * 60;
+    const timeDifference = Math.abs(new Date().getTime() - new Date(comment.createdAt).getTime()) / 1000;
 
     const handleChange = (e) => {
         setCommentInput(e.target.value)
@@ -18,7 +22,7 @@ const EditComment = ({show, setShow, comment, fetchTicket}) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setIsLoading(true);
-        const {ticketId, _id} = comment;
+        const { ticketId, _id } = comment;
         const res = await TicketService.editComment(ticketId, _id, commentInput)
         if (res.data.success === true) {
             setIsLoading(false);
@@ -29,34 +33,50 @@ const EditComment = ({show, setShow, comment, fetchTicket}) => {
 
     useEffect(() => {
         setCommentInput(comment.content);
+        return () => {
+            setSuccess(false);
+        }
+
     }, [comment])
 
     return (
-      <>
-        {
-            show &&
-            <div className="background">
-                <div className="custom-modal">
-                    {
-                        success &&
-                        <div className="d-flex flex-column justify-content-center align-items-center mb-2">
-                            <CheckCircle style={{marginRight:'10px', fontSize:'20px', color:'green'}}></CheckCircle>
-                            Success
+        <>
+            {
+                show &&
+                <div className="background">
+                    <div className="custom-modal">
+                        {
+                            success &&
+                            <div className="success-div">
+                                <CheckCircle style={{ fontSize: '40px', color: 'green' }}></CheckCircle>
+                                Success
+                            </div>
+                        }
+                        <span className="btn-close" onClick={() => setShow(false)}></span>
+                        <div className="d-flex gap-3 mb-2">
+                            <div className="profileInitials rounded-circle d-flex justify-content-center align-items-center">{capitalize(comment.author?.firstName).slice(0, 1) + capitalize(comment.author?.lastName).slice(0, 1)}</div>
+                            <span className="text-color-primary">You</span>
                         </div>
-                    }
-                    <span className="btn-close" onClick={() => setShow(false)}></span>
-                    <TextareaAutosize
-                        className="w-100 p-2 border rounded mt-1"
-                        value={commentInput}
-                        onChange={handleChange}
-                    />
-                    <Button type="submit" className="customBtn mt-1 float-right" disabled={isLoading} onClick={handleSubmit}>
-                        {isLoading ? "Updating..." : "Save"}
-                    </Button>
+
+                        < TextareaAutosize
+                            className="w-100 p-2 border rounded"
+                            value={commentInput}
+                            onChange={handleChange}
+                        />
+                        <div className="d-flex flex-column align-items-end">
+                            <span className="text-sm mt-2 float-right">
+                                Created at {timeDifference > secondsInDay ?
+                                    moment(comment?.createdAt).format('DD-MM-YY h:mm a')
+                                    : capitalize(moment(comment?.createdAt).fromNow(true)) + ' ago'}
+                            </span>
+                            <Button type="submit" className="customBtn mt-3 py-1" style={{ fontSize: '13px' }} disabled={isLoading} onClick={handleSubmit}>
+                                {isLoading ? "Updating..." : "Save"}
+                            </Button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        }
-      </>
+            }
+        </>
     )
 }
 
