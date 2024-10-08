51d2164a04aba19cb891aac4c2631766539781ee
Pawan B K <shiwanbk@gmail.com>
2024-10-08 23:07:47 +1100

    component added

---
+++ src/component/success/SuccessAnimation.jsx
@@ -0,0 +1,15 @@
+import './SuccessAnimation.css'
+const SuccessAnimation = () => {
+    return (
+        <div class="success-checkmark">
+            <div class="check-icon">
+                <span class="icon-line line-tip"></span>
+                <span class="icon-line line-long"></span>
+                <div class="icon-circle"></div>
+                <div class="icon-fix"></div>
+            </div>
+        </div>
+    )
+}
+
+export default SuccessAnimation
