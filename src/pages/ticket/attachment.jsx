a7f9d3a11f20a5257cdfc9b1281657a09d5fac62
Pawan B K <shiwanbk@gmail.com>
2025-01-28 22:03:28 +1100

    Scroll List

--- src/pages/ticket/attachment.jsx
+++ src/pages/ticket/attachment.jsx
@@ -1,42 +1,45 @@
 import { Copy, Download, Trash } from "react-bootstrap-icons";
+import "./attachment.css";
 
 const Attachment = ({attachments, downloadFile, copyToClipBoard, deleteFile}) => {
-    return(
-        <ul>
-            {attachments.map((attachment, index) => (
-                <li
-                    key={attachment?._id || attachment?.name}
-                    className={`p-4 flex justify-between items-center ${
-                    index < attachments.length - 1 ? "border-b" : ""
-                    }`}
-                >
-                    <span className="font-medium">{attachment?.name}</span>
-                    <div className="flex gap-3 text-blue-500">
-                    <span
-                        onClick={() => downloadFile(attachment?._id)}
-                        aria-label="Download file"
-                        className="cursor-pointer"
+    return (
+        <div className="attachment-list">
+            <ul>
+                {attachments.map((attachment, index) => (
+                    <li
+                        key={attachment?._id || attachment?.name}
+                        className={`p-4 flex justify-between items-center ${
+                        index < attachments.length - 1 ? "border-b" : ""
+                        }`}
                     >
-                        <Download />
-                    </span>
-                    <span
-                        onClick={() => copyToClipBoard(attachment?._id)}
-                        aria-label="Copy file ID"
-                        className="cursor-pointer"
-                    >
-                        <Copy />
-                    </span>
-                    <span
-                        onClick={() => deleteFile(attachment?._id)}
-                        aria-label="Delete file"
-                        className="cursor-pointer text-red-500"
-                    >
-                        <Trash />
-                    </span>
-                    </div>
-                </li>
-            ))}
-        </ul>
+                        <span className="font-medium">{attachment?.name}</span>
+                        <div className="flex gap-3 text-blue-500">
+                        <span
+                            onClick={() => downloadFile(attachment?._id)}
+                            aria-label="Download file"
+                            className="cursor-pointer"
+                        >
+                            <Download />
+                        </span>
+                        <span
+                            onClick={() => copyToClipBoard(attachment?._id)}
+                            aria-label="Copy file ID"
+                            className="cursor-pointer"
+                        >
+                            <Copy />
+                        </span>
+                        <span
+                            onClick={() => deleteFile(attachment?._id)}
+                            aria-label="Delete file"
+                            className="cursor-pointer text-red-500"
+                        >
+                            <Trash />
+                        </span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
